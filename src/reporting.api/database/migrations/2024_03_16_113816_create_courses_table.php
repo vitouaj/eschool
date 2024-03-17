0bb1902569eac4a1d8b509a8b33c0990400a6222
vitouaj <vitouaj68@gmail.com>
2024-03-17 14:24:47 +0700

    configure model

---
+++ src/reporting.api/database/migrations/2024_03_16_113816_create_courses_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->uuid('course_id')->default(DB::raw('(UUID())'));
+            $table->string('name');
+            $table->string('created_by');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('courses');
+    }
+};
